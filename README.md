8dd12a29e30e59b4412fba8a9054f3b6f815c0ef
HandSomeGGZ <1300046380@qq.com>
2020-01-04 16:23:09 +0800

    clean project

--- README.md
+++
@@ -1,4 +0,0 @@
-# usage
-- windows 下打开命令提示符
-- cd /dir	#可执行文件所在目录
-- 运行可执行文件并输入参数，如：Project1.exe dec 8 5
--- cmd_def.h
+++
@@ -1,7 +0,0 @@
-#ifdef	__CMD_RUN_H
-#defind __CMD_RUN_H
-
-int register_cmd(char *function_name, char *flag);
-char do_CMD(char *command, char *flag);
-
-#endif
