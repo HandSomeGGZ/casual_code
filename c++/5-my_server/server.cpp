2a9c79e01acc3f222ce5459f478d9d9a0fbb26f7
HandSomeGGZ <1300046380@qq.com>
2020-04-29 22:15:55 +0800

    add raw data process function

--- c++/5-my_server/server.cpp
+++ c++/5-my_server/server.cpp
@@ -14,59 +14,9 @@
 #include <fcntl.h>
 #include <sys/ioctl.h>
 
+#include <thread>
+
 using namespace std;
-
-// class User
-// {
-// private:
-//     /* data */
-//     string user_id;
-//     vector <string> data_to_user;   //此为一个可变的string类型的容器,要发送给用户的数据
-   
-//     string des_id;
-//     vector <string> data_to_des;    //拷贝此数据给des_id的data_to_user
-    
-// public:
-//     bool flag_data_to_user; //true 表示有数据要发送给用户
-//     bool flag_data_to_des;  //true 表示有数据从用户发送过来要发送给其目标用户
-//     User(string new_user_id);
-//     ~User();
-//     int set_data_to_user(string message_data);      //在vector的后面添加一个string的成员
-
-//     int print();
-// };
-
-// User::User(string new_user_id)
-// {
-//     this->user_id=new_user_id;
-//     this->flag_data_to_des=false;
-//     this->flag_data_to_user=false;
-// }
-
-// int User::set_data_to_user(string message_data)
-// {
-//     this->data_to_user.push_back(message_data);     //在数据末尾存入一个数据
-//     return 0;
-// }
-
-// int User::print()
-// {
-//     cout << "user_id:"+this->user_id << endl;
-//     if(!this->data_to_user.empty())
-//     {
-//         for(vector<string>::iterator it = this->data_to_user.begin(); it != this->data_to_user.end(); it++)
-//         {
-//             cout <<"message:"+ *it<<endl;
-//         }
-//         this->data_to_user.clear();     //清除已发送的数据
-//     }
-// }
-
-
-// User::~User()
-// {
-// }
-
 
 #define PORT 8089
 #define MAX_QUE_CONN_NM 5
@@ -76,6 +26,110 @@ struct sockaddr_in server_sockaddr,client_sockaddr;
 
 int sockfd,client_fd,linsten_fd;
 int sin_ssize;
+
+
+
+class User
+{
+private:
+    /* data */
+public:
+    User(/* args */);
+    string id;
+    string des_id;
+    string opration;
+    string message;
+    string passwd;
+    ~User();
+};
+
+User::User(/* args */)
+{
+}
+
+User::~User()
+{
+}
+
+User user;
+
+void pre_process_data(char *buf)        //传过来的buf已经是字符串了
+{
+    string data=buf;
+    int user_id_position=1;
+    int user_id_len=data[user_id_position-1]-'0';    //用户ID的长度
+    user.id=data.substr(user_id_position,user_id_len);
+
+    int opration_position=user_id_len+user_id_position+1;
+    int opration_len=data[opration_position-1]-'0';
+    user.opration=data.substr(opration_position,opration_len);
+    
+    if(user.opration=="login")
+    {
+   
+        int passwd_position=opration_len+opration_position+1;
+        int passwd_len=data[passwd_position-1]-'0';
+        user.passwd=data.substr(passwd_position,passwd_len);
+
+        user.des_id="";
+        user.message="";
+        //获取所有管道文件名/从用户注册文件中读取用户，将ID发送给clint
+    }
+    else if(user.opration=="register")
+    {
+        int passwd_position=opration_len+opration_position+1;
+        int passwd_len=data[passwd_position-1]-'0';
+        user.des_id=data.substr(passwd_position,passwd_len);
+        user.des_id="";
+        user.message="";
+        //创建用户注册文件，以用户ID为文件名，存储用户密码（和好友）
+    }
+    else if(user.opration=="chat")
+    {
+        int des_id_position=opration_len+opration_position+1;
+        int des_id_len=data[des_id_position-1]-'0';
+        user.des_id=data.substr(des_id_position,des_id_len);
+
+        int message_position = des_id_position+des_id_len;
+        user.message=&data[message_position];
+        user.message=user.id+":"+user.message;
+    }
+    else if(user.opration=="quit")
+    {
+        //删除管道文件
+    }
+    cout <<user.id<<endl;
+    cout <<user.opration<<endl;
+    cout << user.des_id<<endl;
+    cout << user.passwd<<endl;
+    cout <<user.message<<endl;
+}
+
+// viod tcp_read_data()
+// {
+//     int recvbytes;
+//     char buf_rev[1024];
+//     cout << "thread is running...";
+//     while(1)
+//     {
+//         if((recvbytes = recv( client_fd,buf_rev,BUFFER_SIZE,0)) == -1)      //阻塞接收
+//         {
+//             cout << "login  faild"<<endl;
+//         }
+//         if(recvbytes > 0)
+//         {
+//             //数据解析函数
+//             buf_rev[recvbytes]='\0';
+//             cout << buf_rev<<endl;      //可分别对账号密码提取，实现密码登录
+            
+//             user.opration=&buf_rev[4];
+            
+//             buf_rev[4]='\0';
+//             user.id = buf_rev;
+//         }
+//     }
+// }
+
 
 int init_server()   //绑定ip和端口等
 {
@@ -110,13 +164,13 @@ int init_server()   //绑定ip和端口等
 		exit(1);
 	}
     cout << "Listening...."<<endl;
-    char buf_rev[1024];
-    char buf_send[1024];
-    int recvbytes;
     
+    char buf_send[1024];
+    char buf_rev[1024];
+    int recvbytes;
     while(1)
     {
-        /*客户端运行直接连接到服务器*/
+        /*客户端运行后点击登录直接连接到服务器*/
         if((client_fd = accept(sockfd,
                 (struct sockaddr*)&client_sockaddr,(socklen_t*)&sin_ssize)) == -1)
         {
@@ -126,7 +180,10 @@ int init_server()   //绑定ip和端口等
 
         if(fork()==0)	//子进程 
         {	
-            //创建线程
+            
+            // thread t(tcp_read_data);    //创建tcp数据接收线程 编译需要加上-std=c++11
+
+
             if((recvbytes = recv( client_fd,buf_rev,BUFFER_SIZE,0)) == -1)      //阻塞接收，等待用户登录
             {
                 cout << "login  faild"<<endl;
@@ -135,24 +192,20 @@ int init_server()   //绑定ip和端口等
             {                               //对数据进行解析提取
                 buf_rev[recvbytes]='\0';
                 cout << buf_rev<<endl;      //可分别对账号密码提取，实现密码登录
-                
-                string operation=&buf_rev[4];
-                
-                buf_rev[4]='\0';
-                string user_id = buf_rev;
+                pre_process_data(buf_rev);
             }
             
-            switch (opration)
-            {
-                case "login":/*验证密码*/break;
-                case "register":/*账号密码写入注册文件*/break;
-                default:break;
-            }
+            // switch (user.opration)
+            // {
+            //     case "login":/*验证密码*/break;
+            //     case "register":/*账号密码写入注册文件*/break;
+            //     default:break;
+            // }
             //验证密码和账户 在此对login 和register进行处理
 
             send(client_fd,"login success",14,0);        //告知客户端登录成功
             //User u_t(user_id);
-            string fifo_path="/tmp/"+user_id;
+            string fifo_path="/tmp/"+user.id;
             //用户上线就创建自己的接收管道，当写入管道不存在说明此用户不存在
             if(mkfifo(fifo_path.c_str(),O_CREAT|O_EXCL)<0)
                 cout<<"can't creat fifo"<<endl;
@@ -173,14 +226,15 @@ int init_server()   //绑定ip和端口等
                 if(recvbytes>0)
                 {
                     //写入des_ID的管道
-                    buf_rev[recvbytes]='\0';
-                    string message=&buf_rev[9];
-                    message=user_id+message;        //告诉目标用户是谁发的消息
-                    buf_rev[9]='\0';
-                    string des_id=&buf_rev[5];
-                    cout << "des_user:"+des_id<<endl <<"message:"+message<<endl;
+                    pre_process_data(buf_rev);
+                    // buf_rev[recvbytes]='\0';
+                    // string message=&buf_rev[9];
+                    //message=user_id+message;        //告诉目标用户是谁发的消息
+                    // buf_rev[9]='\0';
+                    // string des_id=&buf_rev[5];
+                    cout << "des_user:"+user.des_id<<endl <<"message:"+user.message<<endl;
                     //打开对应用户的管道，将数据写入
-                    string des_fifo_path="/tmp/"+des_id;   //合成目的用户管道路径
+                    string des_fifo_path="/tmp/"+user.des_id;   //合成目的用户管道路径
                     int des_fifo_fd=open(des_fifo_path.c_str(),O_RDWR|O_NONBLOCK);      //打开目的用户的管道
                     if(des_fifo_fd<0)
                     {
@@ -189,7 +243,7 @@ int init_server()   //绑定ip和端口等
                     }
                     else
                     {
-                        write(des_fifo_fd,message.c_str(),message.length());//将消息写入管道
+                        write(des_fifo_fd,user.message.c_str(),user.message.length());//将消息写入管道
                     }
                     close(des_fifo_fd);
 
